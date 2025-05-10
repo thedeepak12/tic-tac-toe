f5feb659aef40e0bf311b2e80697e7d0d9917cba
Deepak <coding.deepak12@outlook.com>
2025-05-10 22:24:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+const cells = document.querySelector(".cell");
+const statusText = document.querySelector("#status-text");
+const restartBtn = document.querySelector("#restart-btn");
+
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
+
+function initializeGame() {
+
+}
+
+function cellClicked() {
+
+}
+
+function updateCell(cell, index) {
+
+}
+
+function changePlayer() {
+
+}
+
+function checkWinner() {
+
+}
+
+function restartGame() {
+    
+}
